b85cbed6609f993a8b7750c8f992d3a638b9d249
Enstso <enstso@outlook.fr>
2024-12-21 01:33:55 +0100

    seed

---
+++ prisma/seed.ts
@@ -0,0 +1,20 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient()
+
+async function main(){
+
+    await prisma.project.upsert({
+        where:  {},
+        update:{},
+        create:{
+            
+        }
+    })
+}
+main().then(async ()=>{
+    await prisma.$disconnect()
+}).catch(async (e)=>{
+    await prisma.$disconnect()
+    process.exit(1)
+})
