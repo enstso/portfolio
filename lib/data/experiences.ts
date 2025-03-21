e03ab39e7c688edcb52db3d80bc7e6a59992023a
Enstso <enstso@outlook.fr>
2025-03-21 10:29:08 +0100

    update skills and experiences

--- lib/data/experiences.ts
+++ lib/data/experiences.ts
@@ -1,71 +1,79 @@
 export type ExperienceItem = {
-    name: string;
-    jobOccupation: string;
-    place: string;
-    startDate: Date;
-    endDate: Date | string;
-    description: string[]; 
+  name: string;
+  jobOccupation: string;
+  place: string;
+  startDate: Date;
+  endDate: Date | string;
+  description: string[];
 };
 
 export const ExperiencesList: ExperienceItem[] = [
-    {
-        name: "BNP Paribas Asset Management",
-        jobOccupation: "Software Engineer",
-        place: "Nanterre, Île-de-France, France",
-        startDate: new Date(2024, 9),
-        endDate: "present", 
-        description: ["soon available"]
-    },
-    {
-        name: "Dataventure | Groupe EDG",
-        jobOccupation: "Full Stack Developer",
-        place: "Boulogne-Billancourt, Île-de-France, France",
-        startDate: new Date(2023, 9),
-        endDate: new Date(2024, 9),
-        description: [
-            "Development of landing pages for client campaigns across various sectors (automotive, food, etc.) using CodeIgniter.",
-            "Creation of lead management tools in Symfony, development of lead manipulation scripts in Python, as well as APIs in Node.js and PHP.",
-            "Front-end development using React and Vue.js.",
-            "Project dockerization, implementation of webhooks for data delivery automation with N8N, and understanding of client API documentation.",
-            "Ubiquiti layer three switch configuration"
-        ]
-    },
-    {
-        name: "Cabinet Action",
-        jobOccupation: "Software Developer",
-        place: "Sarcelles, Île-de-France, France",
-        startDate: new Date(2023, 1),
-        endDate: new Date(2024, 2),
-        description: [
-            "Development of a .NET, C# application for managing English courses, aimed at saving time for end users, user management, and document digitization. Implementation of a MYSQL database.",
-            "Documented the project."
-        ]
-    },
-    {
-        name: "Cabinet Action",
-        jobOccupation: "Software Engineer / Desktop Support",
-        place: "Sarcelles, Île-de-France, France",
-        startDate: new Date(2022, 7),
-        endDate: new Date(2022, 12),
-        description: [
-            "Developed and designed an E-learning front-end (JavaScript, Bootstrap) and back-end (Laravel) with an MVC architecture allowing more than 50 customers to practice English.",
-            "Developed a cross-platform Android/IOS mobile application (Flutter) to generate a unique loyalty card for each customer and implemented API with Express and Firebase to interact with the data.",
-            "Setting up an Active Directory including user and group administration, permissions, group policies, domain controller configuration, and audit with Purple Knight.",
-            "Setting up a proxy server (Squid) to block adult, online casino websites and social networks.",
-            "Setting up a Nas server (True Nas) to store company data.",
-            "Assistance and technical support (hardware and software) for employees."
-        ]
-    },
-    {
-        name: "Cabinet Action",
-        jobOccupation: "Web Developer",
-        place: "Sarcelles, Île-de-France, France",
-        startDate: new Date(2022, 5),
-        endDate: new Date(2022, 6),
-        description: [
-            "Developed and designed a website in WordPress.",
-            "Hosting on OVH.",
-            "SEO optimization."
-        ]
-    }
+  {
+    name: "BNP Paribas Asset Management",
+    jobOccupation: "Software Engineer",
+    place: "Nanterre, Île-de-France, France",
+    startDate: new Date(2024, 9),
+    endDate: "present",
+    description: [
+      "Development of an Angular and .NET application, ensuring the implementation of scalable and efficient features to enhance cloud resource management and guarantee application reliability.",
+      "Implementation of unit tests on both the Angular frontend (using Jasmine/Karma) and the .NET backend (with NUnit), significantly improving stability and reducing regressions.",
+      "Set up a PostgreSQL script using a cursor to restore coherence across records, ensuring data consistency and compliance with ACID principles.",
+      "Data analysis from Excel files to detect inconsistencies and define a structured fix strategy, ensuring data integrity.",
+      "Optimization of database structures by improving table design and implementing migration scripts to correct existing data issues, ultimately enhancing query performance and system reliability.",
+      "Understanding of user requirements.",
+      "Advanced training in cloud technologies",
+    ],
+  },
+  {
+    name: "Dataventure | Groupe EDG",
+    jobOccupation: "Full Stack Developer",
+    place: "Boulogne-Billancourt, Île-de-France, France",
+    startDate: new Date(2023, 9),
+    endDate: new Date(2024, 9),
+    description: [
+      "Development of landing pages for client campaigns across various sectors (automotive, food, etc.) using CodeIgniter.",
+      "Creation of lead management tools in Symfony, development of lead manipulation scripts in Python, as well as APIs in Node.js and PHP.",
+      "Front-end development using React and Vue.js.",
+      "Project dockerization, implementation of webhooks for data delivery automation with N8N, and understanding of client API documentation.",
+      "Ubiquiti layer three switch configuration",
+    ],
+  },
+  {
+    name: "Cabinet Action",
+    jobOccupation: "Software Developer",
+    place: "Sarcelles, Île-de-France, France",
+    startDate: new Date(2023, 1),
+    endDate: new Date(2024, 2),
+    description: [
+      "Development of a .NET, C# application for managing English courses, aimed at saving time for end users, user management, and document digitization. Implementation of a MYSQL database.",
+      "Documented the project.",
+    ],
+  },
+  {
+    name: "Cabinet Action",
+    jobOccupation: "Software Engineer / Desktop Support",
+    place: "Sarcelles, Île-de-France, France",
+    startDate: new Date(2022, 7),
+    endDate: new Date(2022, 12),
+    description: [
+      "Developed and designed an E-learning front-end (JavaScript, Bootstrap) and back-end (Laravel) with an MVC architecture allowing more than 50 customers to practice English.",
+      "Developed a cross-platform Android/IOS mobile application (Flutter) to generate a unique loyalty card for each customer and implemented API with Express and Firebase to interact with the data.",
+      "Setting up an Active Directory including user and group administration, permissions, group policies, domain controller configuration, and audit with Purple Knight.",
+      "Setting up a proxy server (Squid) to block adult, online casino websites and social networks.",
+      "Setting up a Nas server (True Nas) to store company data.",
+      "Assistance and technical support (hardware and software) for employees.",
+    ],
+  },
+  {
+    name: "Cabinet Action",
+    jobOccupation: "Web Developer",
+    place: "Sarcelles, Île-de-France, France",
+    startDate: new Date(2022, 5),
+    endDate: new Date(2022, 6),
+    description: [
+      "Developed and designed a website in WordPress.",
+      "Hosting on OVH.",
+      "SEO optimization.",
+    ],
+  },
 ];
--- lib/data/skills.tsx
+++ lib/data/skills.tsx
@@ -1,171 +1,174 @@
 import {
-    FaAngular,
-    FaBootstrap,
-    FaCss3,
-    FaDocker,
-    FaGithub,
-    FaHtml5,
-    FaLaravel,
-    FaLinux,
-    FaPhp,
-    FaPython,
-    FaReact,
-    FaSlack,
-    FaSymfony,
-    FaUbuntu,
-    FaWindows,
-    FaWordpress,
-  } from "react-icons/fa";
-  import { FaJs } from "react-icons/fa";
-  import { TbBrandCSharp } from "react-icons/tb";
-  import {
-    SiAdonisjs,
-    SiAnsible,
-    SiApache,
-    SiApachespark,
-    SiBurpsuite,
-    SiCisco,
-    SiCloudron,
-    SiCodeigniter,
-    SiContabo,
-    SiDotnet,
-    SiExpress,
-    SiFlask,
-    SiKubernetes,
-    SiMacos,
-    SiMariadb,
-    SiMetasploit,
-    SiMongodb,
-    SiMysql,
-    SiN8N,
-    SiNginx,
-    SiOvh,
-    SiPfsense,
-    SiPostgresql,
-    SiRabbitmq,
-    SiRedis,
-    SiTailwindcss,
-    SiTypescript,
-    SiUbiquiti,
-    SiWireshark,
-  } from "react-icons/si";
-  import { FaDebian } from "react-icons/fa6";
-  import {
-
-    RiNextjsFill,
-    RiNotionFill,
-  } from "react-icons/ri";
-  import { VscAzure } from "react-icons/vsc";
-  import { AiOutlineAudit } from "react-icons/ai";
+  FaAngular,
+  FaBootstrap,
+  FaCss3,
+  FaDocker,
+  FaGithub,
+  FaHtml5,
+  FaLaravel,
+  FaLinux,
+  FaPhp,
+  FaPython,
+  FaReact,
+  FaSlack,
+  FaSymfony,
+  FaUbuntu,
+  FaWindows,
+  FaWordpress,
+} from "react-icons/fa";
+import { FaJs } from "react-icons/fa";
+import { TbBrandCSharp } from "react-icons/tb";
+import {
+  SiAdonisjs,
+  SiAnsible,
+  SiApache,
+  SiApachespark,
+  SiBurpsuite,
+  SiCisco,
+  SiCloudron,
+  SiCodeigniter,
+  SiContabo,
+  SiDotnet,
+  SiExpress,
+  SiFlask,
+  SiKubernetes,
+  SiMacos,
+  SiMariadb,
+  SiMetasploit,
+  SiMongodb,
+  SiMysql,
+  SiN8N,
+  SiNginx,
+  SiOvh,
+  SiPfsense,
+  SiPostgresql,
+  SiRabbitmq,
+  SiRedis,
+  SiTailwindcss,
+  SiTypescript,
+  SiUbiquiti,
+  SiWireshark,
+  SiNestjs,
+  SiGraphql,
+  SiK6,
+  SiSelenium 
+} from "react-icons/si";
+import { FaDebian } from "react-icons/fa6";
+import { RiNextjsFill, RiNotionFill } from "react-icons/ri";
+import { VscAzure } from "react-icons/vsc";
+import { AiOutlineAudit } from "react-icons/ai";
 import { ReactNode } from "react";
 
 export type SkillsItems = {
-    icon: ReactNode;
-    name: string;
-  };
-  
-  export type SkillsList = {
-    domain: string;
-    iconsItems: SkillsItems[];
-  };
+  icon: ReactNode;
+  name: string;
+};
 
+export type SkillsList = {
+  domain: string;
+  iconsItems: SkillsItems[];
+};
 
-  export const skillsItems: SkillsList[] = [
-    {
-      domain: "Languages",
-      iconsItems: [
-        { icon: <FaHtml5 />, name: "HTML" },
-        { icon: <FaCss3 />, name: "CSS" },
-        { icon: <FaJs />, name: "JavaScript" },
-        { icon: <SiTypescript />, name: "TypeScript" },
-        { icon: <FaPython />, name: "Python" },
-        { icon: <TbBrandCSharp />, name: "C#" },
-        { icon: <FaPhp />, name: "PHP" },
-      ],
-    },
-    {
-      domain: "Frameworks",
-      iconsItems: [
-        { icon: <FaReact />, name: "React" },
-        { icon: <FaAngular />, name: "Angular" },
-        { icon: <RiNextjsFill />, name: "Next.js" },
-        { icon: <SiDotnet />, name: ".NET" },
-        { icon: <FaSymfony />, name: "Symfony" },
-        { icon: <FaLaravel />, name: "Laravel" },
-        { icon: <SiCodeigniter />, name: "CodeIgniter" },
-        { icon: <SiAdonisjs />, name: "Adonis.js" },
-        { icon: <SiExpress />, name: "Express" },
-        { icon: <SiFlask />, name: "Flask" },
-        { icon: <SiTailwindcss />, name: "Tailwind CSS" },
-        { icon: <FaBootstrap />, name: "Bootstrap" },
-      ],
-    },
-    {
-      domain: "Databases",
-      iconsItems: [
-        { icon: <SiMysql />, name: "MySQL" },
-        { icon: <SiMariadb />, name: "MariaDB" },
-        { icon: <SiPostgresql />, name: "PostgreSQL" },
-        { icon: <SiMongodb />, name: "MongoDB" },
-        { icon: <SiRedis />, name: "Redis" },
-      ],
-    },
-    {
-      domain: "DevOps",
-      iconsItems: [
-        { icon: <FaGithub />, name: "GitHub" },
-        { icon: <FaDocker />, name: "Docker" },
-        { icon: <SiKubernetes />, name: "Kubernetes" },
-        { icon: <SiAnsible />, name: "Ansible" },
-        { icon: <VscAzure />, name: "Azure" },
-        { icon: <SiApachespark/>, name: "Apache Spark"}
-      ],
-    },
-    {
-      domain: "Cybersecurity",
-      iconsItems: [
-        { icon: <SiBurpsuite />, name: "Burp Suite" },
-        { icon: <SiMetasploit />, name: "Metasploit" },
-        { icon: <AiOutlineAudit />, name: "Audit" },
-      ],
-    },
-    {
-      domain: "Network",
-      iconsItems: [
-        { icon: <SiCisco />, name: "Cisco" },
-        { icon: <SiUbiquiti />, name: "Ubiquiti" },
-        { icon: <SiPfsense />, name: "Pfsense" },
-        { icon: <SiWireshark/>, name: "Wireshark"}
-      ],
-    },
-    {
-      domain: "System Services",
-      iconsItems: [
-        { icon: <SiApache />, name: "Apache" },
-        { icon: <SiNginx />, name: "Nginx" },
-        { icon: <SiRabbitmq />, name: "RabbitMQ" },
-      ],
-    },
-    {
-      domain: "OS and Distributions",
-      iconsItems: [
-        { icon: <FaLinux />, name: "Linux" },
-        { icon: <FaWindows />, name: "Windows" },
-        { icon: <SiMacos />, name: "MacOS" },
-        { icon: <FaDebian />, name: "Debian" },
-        { icon: <FaUbuntu />, name: "Ubuntu" },
-      ],
-    },
-    {
-      domain: "Tools and Others",
-      iconsItems: [
-        { icon: <SiN8N />, name: "N8N" },
-        { icon: <SiCloudron />, name: "Cloudron" },
-        { icon: <FaWordpress />, name: "WordPress" },
-        { icon: <SiOvh />, name: "OVH" },
-        { icon: <SiContabo />, name: "Contabo" },
-        { icon: <RiNotionFill />, name: "Notion" },
-        { icon: <FaSlack />, name: "Slack" },
-      ],
-    },
-  ];
+export const skillsItems: SkillsList[] = [
+  {
+    domain: "Languages",
+    iconsItems: [
+      { icon: <FaHtml5 />, name: "HTML" },
+      { icon: <FaCss3 />, name: "CSS" },
+      { icon: <FaJs />, name: "JavaScript" },
+      { icon: <SiTypescript />, name: "TypeScript" },
+      { icon: <FaPython />, name: "Python" },
+      { icon: <TbBrandCSharp />, name: "C#" },
+      { icon: <FaPhp />, name: "PHP" },
+    ],
+  },
+  {
+    domain: "Frameworks",
+    iconsItems: [
+      { icon: <FaReact />, name: "React" },
+      { icon: <FaAngular />, name: "Angular" },
+      { icon: <RiNextjsFill />, name: "Next.js" },
+      { icon: <SiDotnet />, name: ".NET" },
+      { icon: <FaSymfony />, name: "Symfony" },
+      { icon: <SiNestjs />, name: "NestJS" },
+      { icon: <SiGraphql />, name: "GraphQL" },
+      { icon: <FaLaravel />, name: "Laravel" },
+      { icon: <SiCodeigniter />, name: "CodeIgniter" },
+      { icon: <SiAdonisjs />, name: "Adonis.js" },
+      { icon: <SiExpress />, name: "Express" },
+      { icon: <SiFlask />, name: "Flask" },
+      { icon: <SiTailwindcss />, name: "Tailwind CSS" },
+      { icon: <FaBootstrap />, name: "Bootstrap" },
+    ],
+  },
+  {
+    domain: "Databases",
+    iconsItems: [
+      { icon: <SiMysql />, name: "MySQL" },
+      { icon: <SiMariadb />, name: "MariaDB" },
+      { icon: <SiPostgresql />, name: "PostgreSQL" },
+      { icon: <SiMongodb />, name: "MongoDB" },
+      { icon: <SiRedis />, name: "Redis" },
+    ],
+  },
+  {
+    domain: "DevOps",
+    iconsItems: [
+      { icon: <FaGithub />, name: "GitHub" },
+      { icon: <FaDocker />, name: "Docker" },
+      { icon: <SiKubernetes />, name: "Kubernetes" },
+      { icon: <SiAnsible />, name: "Ansible" },
+      { icon: <VscAzure />, name: "Azure" },
+      { icon: <SiApachespark />, name: "Apache Spark" },
+    ],
+  },
+  {
+    domain: "Cybersecurity",
+    iconsItems: [
+      { icon: <SiBurpsuite />, name: "Burp Suite" },
+      { icon: <SiMetasploit />, name: "Metasploit" },
+      { icon: <AiOutlineAudit />, name: "Audit" },
+    ],
+  },
+  {
+    domain: "Network",
+    iconsItems: [
+      { icon: <SiCisco />, name: "Cisco" },
+      { icon: <SiUbiquiti />, name: "Ubiquiti" },
+      { icon: <SiPfsense />, name: "Pfsense" },
+      { icon: <SiWireshark />, name: "Wireshark" },
+    ],
+  },
+  {
+    domain: "System Services",
+    iconsItems: [
+      { icon: <SiApache />, name: "Apache" },
+      { icon: <SiNginx />, name: "Nginx" },
+      { icon: <SiRabbitmq />, name: "RabbitMQ" },
+    ],
+  },
+  {
+    domain: "OS and Distributions",
+    iconsItems: [
+      { icon: <FaLinux />, name: "Linux" },
+      { icon: <FaWindows />, name: "Windows" },
+      { icon: <SiMacos />, name: "MacOS" },
+      { icon: <FaDebian />, name: "Debian" },
+      { icon: <FaUbuntu />, name: "Ubuntu" },
+    ],
+  },
+  {
+    domain: "Tools and Others",
+    iconsItems: [
+      { icon: <SiN8N />, name: "N8N" },
+      { icon: <SiK6 />, name: "K6" },
+      { icon: <SiSelenium />, name: "Selenium" },
+      { icon: <SiCloudron />, name: "Cloudron" },
+      { icon: <FaWordpress />, name: "WordPress" },
+      { icon: <SiOvh />, name: "OVH" },
+      { icon: <SiContabo />, name: "Contabo" },
+      { icon: <RiNotionFill />, name: "Notion" },
+      { icon: <FaSlack />, name: "Slack" },
+    ],
+  },
+];
